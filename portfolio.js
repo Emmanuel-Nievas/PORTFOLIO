1c10c6c1d70d0b74b27b0734b6a294fce09a454f
JAIR MACRE <jairemmanueljm@gmail.com>
2022-03-14 16:36:48 -0300

    se agrega el proyecto

---
+++ portfolio.js
@@ -0,0 +1,47 @@
+
+/*
+let ar = ['emm','lost','dark'];
+
+let solucion = []
+
+
+function posicion(x){
+
+
+return solucion = ar.indexOf(x)
+
+
+}
+
+console.log(solucion)
+
+
+
+
+let num = parseInt(prompt('Ingrese numero decimal'));
+
+let result = num.toString(2)
+
+console.log(result) */
+
+
+
+function decimalAbinario(a,b){
+
+let sum = a + b;
+
+let result = sum.toString(2);
+
+let binario = result.toString();
+
+return binario
+}
+
+console.log(binario);
+
+
+
+
+
+
+
